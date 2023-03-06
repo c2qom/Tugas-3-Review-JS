1347e3b9e6d197945abdfe56eb3858a92f63111c
c2qom <c2kkqom@gmail.com>
2023-03-06 15:30:20 +0700

    Add files via upload
    Tugas3 Review js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Tugas 3. Refiew-js
+let s = "";
+let c3 = 0, c5 = 0;
+for (var i = 1; i <= 100; i++) {
+    c3++;
+    c5++;
+    if (c3 == 3) {
+        s += "fizz";
+        c3 = 0;
+    }
+    if (c5 == 5) {
+        s += "buzz";
+        c5 = 0;
+    }
+    if (s.length == 0)
+        console.log(i);
+    else
+        console.log(s);
+    s = "";
+}
+ 
+// FizzBuzz
